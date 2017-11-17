0e4086104618fcb54fc7d0c9b7871e624f0d54d1
Saifullah Saifi <saifi@csdamu.com>
2017-11-17 11:58:29 +0530

    changed for account

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.post('/update', function(req, res) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
-            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 ,Industry = $4  WHERE LOWER(Name) = LOWER($2) AND  AND LOWER(Email) = LOWER($3)',
+            [req.body.phone.trim(), req.body.firstName.trim(), req.body.email.trim(),req.body.industry.trim() ],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (Phone,HomePhone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $1, $2, $3, $4, $5)',
-                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
-                  function(err, result) {
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, Name, Email, Industry) VALUES ($1, $1, $2, $3, $4)',
+                  [req.body.phone.trim(), req.body.firstName.trim(), req.body.email.trim(),req.body.industry.trim() ],
+				  function(err, result) {
                     done();
                     if (err) {
                         res.status(400).json({error: err.message});
